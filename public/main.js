14493b4281d061cabad56be5735e74dee15750a3
JenSeoul <n.waves429@gmail.com>
2021-03-07 11:22:20 +0900

    add server and public directory

---
+++ public/main.js
@@ -0,0 +1,56 @@
+const albumDivs = document.querySelectorAll('.albumbox');
+const controlPlay = document.querySelector('#controlplay');
+const controlStop = document.querySelector('#controlstop');
+const navPlay = document.querySelector('#navplay')
+const navStop = document.querySelector('#navstop')
+const allSongs = document.querySelectorAll('audio');
+const allItags = document.querySelectorAll('i');
+const arrayItags = Array.from(allItags);
+const mainItags = arrayItags.slice(2,10);
+
+
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+tl.to(".text",{y:"0", duration:1, stagger: 0.25});
+tl.to(".slider", {y:"-100%", duration:1, delay:0.5});
+tl.to(".intro", {y:"-100%", duration:1, delay:0.5}, "-=1");
+
+mainItags.forEach(itags=>{
+    itags.addEventListener('click', playSong);
+    function playSong(e){
+        const playButton = e.target;
+        const div = playButton.parentElement.children;
+        const anChor = Array.from(div)[0];
+        const song = Array.from(anChor.children)[2];
+        allSongs.forEach(songs=>songs.pause());
+        mainItags.forEach(itags=>{
+            itags.style.color ='#363636'
+        });
+        song.play();
+        controlPlay.style.color = '#DB1210';
+        navPlay.style.color = '#DB1210';
+        playButton.style.color = '#DB1210';
+        controlStop.addEventListener('click', ()=>{
+            allSongs.forEach(songs=>songs.pause())
+            song.pause()
+            controlStop.style.color = '#DB1210';
+            playButton.style.color= '#363636';
+            controlPlay.style.color = '#fff';
+            setTimeout(()=>{controlStop.style.color ='#fff'}, 1500)
+        })
+        navStop.addEventListener('click',()=>{
+            allSongs.forEach(songs=>songs.pause())
+            song.pause()
+            playButton.style.color= '#363636';
+            navStop.style.color ='#DB1210';
+            navPlay.style.color = '#fff';
+            setTimeout(()=>{navStop.style.color = '#fff'},1500)
+        })
+        allSongs.forEach(songs=>{
+            songs.onended = function(){
+                playButton.style.color= '#363636';
+                navPlay.style.color = '#fff';
+                controlPlay.style.color ='#fff';
+            }
+        })
+    }
+})
---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req,res) => {
+
+    // Build file path
+    const filePath = path.join(__dirname, '')
+
+
+
+})
